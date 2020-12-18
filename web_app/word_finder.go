573f2620c7769c36dc21dec77447c397a3d61072
abhishek <anshu98@outlook.com>
2020-12-18 22:34:32 +0530

    Concurrency added

---
+++ web_app/word_finder.go
@@ -0,0 +1,39 @@
+// substring_matching.go
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Make HTTP GET request
+	fmt.Println("Enter Web URLs: ")
+	// var then variable name then variable type
+	var URL string
+	// Taking input from user
+	fmt.Scanln(&URL)
+	response, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	// Get the response body as a string
+	dataInBytes, err := ioutil.ReadAll(response.Body)
+	pageContent := string(dataInBytes)
+	// Find a substr
+	var subStr string
+	fmt.Println("Enter the Keyword you wanna find: ")
+	fmt.Scanln(&subStr)
+	hasSubstr := strings.Contains(pageContent, subStr)
+	if hasSubstr == false {
+		fmt.Println("Try another web pal !!!!")
+		os.Exit(0)
+	} else {
+		fmt.Println("Gotcha!!..web contains that keyword punk")
+	}
+}
