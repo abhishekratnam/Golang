5b6b8f9e795d553447c942d0b90b7f957a651930
root <anshu98@outlook.com>
2021-03-05 08:36:59 +0530

    add ons

---
+++ post_connector.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "jarvis"
+	dbname   = "abhi" //ALTER USER postgres PASSWORD 'jarvis';
+)
+
+type User struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Age      int64  `json:"age"`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+}
